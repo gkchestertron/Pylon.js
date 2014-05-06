6fc79854c73153c6f1321edae227fa7fcaca018b
John Fellman <john.fellman@avrs.com>
2014-05-05 22:51:57 -0700

    sloppy start to animation

--- js/Pylon.js
+++ js/Pylon.js
@@ -3,29 +3,56 @@
 
     Pylon.begets = function (children) {
         for (var child in children) {
-            var Surrogate = function () {};
-                this[child] = function () {};
+            var self = this;
+                Surrogate = function () {},
+                self[child] = function () {};
             Surrogate.prototype = this.prototype;
-            this[child].prototype = new Surrogate();
-            this[child].begets = this.begets;
-            this[child].create = this.create;
-            this[child].group = children[child].group || Pylon.createGroup();
+            self[child].prototype = new Surrogate();
+            self[child].begets = this.begets.bind(self[child]);
+            self[child].create = this.create.bind(self[child]);
+            self[child].group = children[child].group || Pylon.createGroup();
+
+            if (children[child].context) {
+                children[child].context.layers = children[child].context.layers || [];
+                children[child].context.startAnimation = function () {
+                    var canvas = children[child].context.canvas;
+                    children[child].context.interval = window.setInterval(function () {
+                        children[child].context.clearRect(0, 0, canvas.width, canvas.height);
+                        for (var layer in children[child].context.layers) {
+                            children[child].context.layers[layer]();
+                        }
+                    }, 33);
+                }
+
+                children[child].context.stopAnimation = function () {
+                    window.clearInterval(children[child].context.interval);
+                }
+            }
+
             for (var key in children[child]) {
-                this[child].prototype[key] = children[child][key];
+                self[child].prototype[key] = children[child][key];
             }
         }
-        return this[child];
+        return self[child];
     }
 
     Pylon.create = function (options) {
-        var thing = function () {};
-        thing.prototype = this.prototype;
-        thing = new thing();
+        var thing = function () {},
+            newThing; 
+        thing.prototype = new this;
+        newThing = new thing();
         for (var prop in options) {
-            thing.set(prop, options[prop]);
+            if (typeof(options[prop]) === "function") {
+                // newThing.set(prop, options[prop].bind(newThing));
+                newThing[prop] = options[prop].bind(newThing);
+            } else {
+                // newThing.set(prop, options[prop]);
+                newThing[prop] = options[prop];
+            }
+
         }
-        this.group.things.push(thing);
-        return thing;
+        this.group.things.push(newThing);
+        return newThing;
     }
 
     Pylon.createGroup = function () {
@@ -38,6 +65,11 @@
     }
     
     Pylon.prototype = {
+        animate: function () {
+            if (this.context && this.draw) {
+                this.context.layers.push(this.draw);
+            }
+        },
         attributes: {},
         get: function (attr) {
             return this.attributes[attr];
@@ -47,18 +79,32 @@
         },
         name: "default",
         render: function () {},
-        draw: function () {},
-        context: {}, // contains reference to animation timer?
         navigation: "string" //or function representing context
     };
 })(this);
 
 NA = Pylon;
-
-NA.begets({
-    Animal: {
-        run: function () {
-            console.log('run!');
+$(document).ready(function () {
+    NA.begets({
+        Animal: {
+            context: $('canvas')[0].getContext("2d")
         }
-    }
+    });
+    
+    animal1 = NA.Animal.create({
+        draw: function () {
+            this.context.fillStyle = "#ff0000"
+            var x = Math.random() * 500;
+            this.context.fillRect(0,0,x,x);
+        },
+        name: "thing1"
+    })
+    animal2 = NA.Animal.create({
+        draw: function () {
+            this.context.fillStyle = "#000000"
+            var x = Math.random() * 500;
+            this.context.fillRect(0,0,x,x);
+        },
+        name: "thing2"
+    })
 });
